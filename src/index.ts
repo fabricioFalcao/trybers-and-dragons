d2a909b1b7818480ccdabf7b823106d8476834c9
fabricioFalcao <fabriciohfd@gmail.com>
2024-03-05 14:13:13 -0300

    Bonus: playing with Pitty

--- src/index.ts
+++ src/index.ts
@@ -16,6 +16,7 @@ soloLeveling(4, player1);
 
 const monster1 = new Monster();
 const monster2 = new Dragon();
+// const dragon2 = new Dragon();
 
 const pvp = new PVP(player2, player3);
 
@@ -24,6 +25,17 @@ const pve = new PVE(player1, [monster1, monster2]);
 function runBattles(battles: Battle[]) {
   battles.forEach((battle) => battle.fight());
 }
+
+const Pitty = new Character('Pitty');
+soloLeveling(20, Pitty);
+
+// const pvPitty = new PVP(Pitty, player1).fight();
+// const evPitty = new PVE(Pitty, [monster1, monster2, dragon2]).fight();
+
+// console.log({
+//   pvPitty,
+//   evPitty,
+// });
 
 export {
   player1,
